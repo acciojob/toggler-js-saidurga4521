0171fe08f1b0ffac3a88cddae8947e68c1db2469
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-04-23 14:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const toggles=document.querySelectorAll('.toggle');
+let selected=[];
+toggles.forEach(toggle=>{
+	toggle.addEventListener('change',()=>{
+		if(toggle.checked){
+			selected.push(toggle);
+			if(selected.length>2){
+				const oldest=selected.shift();
+				oldest.checked=false;
+			}
+		}else{
+			selected=selected.filter(t => t!==toggle);
+			}
+	});
+});
